c9b5b9ee6f595aeda94eb36f8b35b400e1a0ebba
Heet <heetmistry@gmail.com>
2022-01-20 15:34:18 +0530

    Added the home routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+      <Routes>
+          <Route path='/' element={<Home />} >
+
+          </Route>
+      </Routes>
+  </Router>,
   document.getElementById('root')
 );
+
+function Home(){
+  return (
+    <div>
+      <h1>Home Route</h1>
+    </div>
+  )
+}
+
+function Learn(){
+  return (
+    <div>
+      <h1>Learn</h1>
+      <h4>All courses are listed are here</h4>
+    </div>
+  )
+}
