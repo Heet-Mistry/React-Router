5aac7fd61ca890ca2f044387c0ad01e88748100a
Heet <heetmistry@gmail.com>
2022-01-20 16:53:55 +0530

    Added the Navlink

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {BrowserRouter as Router,Routes,Route,Navigate,Link,Outlet,useParams} from 'react-router-dom'
+import {BrowserRouter as Router,Routes,Route,Navigate,Link,Outlet,useParams,NavLink} from 'react-router-dom'
 
 
 ReactDOM.render(
@@ -11,6 +11,7 @@ ReactDOM.render(
           <Route path='/myapps' element={<Navigate replace to='/learn'/>} />
          
           <Route path='/learn' element={<Learn />} >
+                   <Route path=':id' element={<Navigate replace to='/'/>}/>
                    <Route path='courses' element={<Courses/>} >
                             <Route path=':id' element={<CourseId />}/>
                    </Route>
@@ -24,14 +25,13 @@ ReactDOM.render(
 
 function CourseId(){
 
-  const {id} = useParams()
+  const {id} = useParams();
   return(
     <div>
       <h1>{`Your Url is ${id}`}</h1>
     </div>
   )
 }
-
 
 function Home(){
   return (
@@ -55,10 +55,19 @@ function Learn(){
 }
 
 function Courses(){
+  const coursesList=['react','JS','Angular','Machine-Learning','EmbeddedMachineLearning'];
+  const randomCourse = coursesList[Math.floor(Math.random()*coursesList.length)];
   return (
     <div>
       <h1>Courses List</h1>
       <h1>Courses Card</h1>
+      <NavLink style={({isActive})=>{
+        return{ backgroundColor:isActive?"rebeccapurple":"cyan"}
+      }} 
+      to={`/learn/courses/${randomCourse}`}>{randomCourse}</NavLink>
+
+      <NavLink className='btn btn-dark' to={`/learn/courses/test`}>Test</NavLink>
+
       <Outlet />
     </div>
   )
